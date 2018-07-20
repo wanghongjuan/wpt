704c063c929d81ccaac3af25f459d4f011bd4673
Timothy Gu <timothygu99@gmail.com>
2018-07-20 18:36:59 -0400

    document open steps: More event listener tests

--- html/webappapis/dynamic-markup-insertion/opening-the-input-stream/event-listeners.window.js
+++ html/webappapis/dynamic-markup-insertion/opening-the-input-stream/event-listeners.window.js
@@ -2,13 +2,80 @@ test(t => {
   const frame = document.body.appendChild(document.createElement("iframe")),
         body = frame.contentDocument.body;
   t.add_cleanup(() => frame.remove());
+  const div = body.appendChild(frame.contentDocument.createElement("div"));
+  div.addEventListener("click", t.unreached_func("element event listener not removed"));
+  frame.contentDocument.open();
+  div.click();
+  frame.contentDocument.close();
+}, "Standard event listeners are to be removed");
+
+test(t => {
+  const frame = document.body.appendChild(document.createElement("iframe")),
+        body = frame.contentDocument.body;
+  t.add_cleanup(() => frame.remove());
   frame.contentDocument.addEventListener("x", t.unreached_func("document event listener not removed"));
   body.addEventListener("x", t.unreached_func("body event listener not removed"));
   frame.contentDocument.open();
   frame.contentDocument.dispatchEvent(new Event("x"));
   body.dispatchEvent(new Event("x"));
   frame.contentDocument.close();
-}, "Event listeners are to be removed");
+}, "Custom event listeners are to be removed");
+
+test(t => {
+  const frame = document.body.appendChild(document.createElement("iframe")),
+        body = frame.contentDocument.body;
+  t.add_cleanup(() => frame.remove());
+  // Focus on the current window so that the frame's window is blurred.
+  window.focus();
+  assert_false(frame.contentDocument.hasFocus());
+  frame.contentWindow.addEventListener("focus", t.unreached_func("window event listener not removed"));
+  body.onfocus = t.unreached_func("body event listener not removed");
+  frame.contentDocument.open();
+  // Pending resolution from https://github.com/whatwg/html/issues/3836.
+  // assert_equals(body.onfocus, null);
+  frame.contentWindow.focus();
+  frame.contentDocument.close();
+}, "Standard event listeners are to be removed from Window");
+
+test(t => {
+  const frame = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => frame.remove());
+  frame.contentWindow.addEventListener("x", t.unreached_func("window event listener not removed"));
+  frame.contentDocument.open();
+  frame.contentWindow.dispatchEvent(new Event("x"));
+  frame.contentDocument.close();
+}, "Custom event listeners are to be removed from Window");
+
+test(t => {
+  const frame = document.body.appendChild(document.createElement("iframe")),
+        body = frame.contentDocument.body;
+  t.add_cleanup(() => frame.remove());
+  const div = body.appendChild(frame.contentDocument.createElement("div"));
+  div.onclick = t.unreached_func("element event listener not removed");
+  frame.contentDocument.open();
+  // Pending resolution from https://github.com/whatwg/html/issues/3836.
+  // assert_equals(div.onclick, null);
+  const e = frame.contentDocument.createEvent("mouseevents")
+  e.initEvent("click", false, false);
+  div.dispatchEvent(e);
+  frame.contentDocument.close();
+}, "IDL attribute event handlers are to be removed");
+
+test(t => {
+  const frame = document.body.appendChild(document.createElement("iframe")),
+        body = frame.contentDocument.body;
+  t.add_cleanup(() => frame.remove());
+  const div = body.appendChild(frame.contentDocument.createElement("div"));
+  div.setAttribute("onclick", "throw new Error('element event listener not removed')");
+  assert_not_equals(div.onclick, null);
+  frame.contentDocument.open();
+  // Pending resolution from https://github.com/whatwg/html/issues/3836.
+  // assert_equals(div.onclick, null);
+  const e = frame.contentDocument.createEvent("mouseevents")
+  e.initEvent("click", false, false);
+  div.dispatchEvent(e);
+  frame.contentDocument.close();
+}, "Content attribute event handlers are to be removed");
 
 test(t => {
   const frame = document.body.appendChild(document.createElement("iframe"));
